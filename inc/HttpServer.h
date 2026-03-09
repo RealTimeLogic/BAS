2a6ec83a4eb22e58a4a30aee7329e9e9378a3006
RTL <24235755+Telemechanics@users.noreply.github.com>
2026-03-09 14:47:36 -0700

    Updated to V5729

--- inc/HttpServer.h
+++ inc/HttpServer.h
@@ -36,9 +36,9 @@
  *
  */
 
-#define BASLIB_VER_NO 5729
+#define BASLIB_VER_NO 5746
 #define BASLIB_VER_M(x) #x
-#define BASLIB_VER BASLIB_VER_M(5729)
+#define BASLIB_VER BASLIB_VER_M(5746)
 
 /*! \page HttpDirVolatileMem Volatile/temporary memory used as name in a HttpDir/HttpPage
 
